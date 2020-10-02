cc1e9867c50a6bbe9e92fc1f9bb75fd5401c76a2
Hilario-gomez <71901200+Hilario-gomez@users.noreply.github.com>
2020-10-02 21:43:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,14 @@ def login():
 def about():
     return 'QnA App | About.'
 
+@app.route('/Ask a question')
+def ask():
+    return 'QnA App  |  Ask a question'
+
+@app.route('/View a question')
+def view():
+    return 'QnA App  |  View a question'
+
 
 if __name__ == "__main__":
     app.run(debug = True)

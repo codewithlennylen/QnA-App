61bf792eea2e7a4133c6fcdf06cac22902abb186
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-10-03 21:52:04 +0300

    Merging Frontend With Backend

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'QnA App | Homepage'
+    return render_template('index.html')
 
 @app.route('/myquestions')
 def myquestions():

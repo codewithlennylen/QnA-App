c9d431e55227860e80b295db4e3162a193fbc24d
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-10-12 21:35:08 +0300

    Passed questions-dictionary to index.html

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ questions = {
     "Is pirate-bay illegal": "Computer software",
     "The blue screen of death!": "Computer software",
     "What is linux?": "Computing",
-    "How do you restart a computer?": "Computer software",
+    "How do you restart a computer??": "Computer software",
     "How do you access the dark web?": "Networking",
     "Which is the best VPN to use?": "Security",
     "How do I know if my computer has been hacked?": "Security",

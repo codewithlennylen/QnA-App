031cc29c89f6ff8b819578f41196819bfe451f09
Hilario-gomez <71901200+Hilario-gomez@users.noreply.github.com>
2020-10-12 09:14:41 +0100

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,11 @@
+questions={
+    "How do you restart a computer?":"Networking",
+    "What programming language should i learn":"Computer programming",
+    "What is linux":"Computing",
+    "Is pirate-bay illegal":"Computer software",
+    "The blue screen of death!":"Computer software",
+    "What is linux?":"Computing",
+    "How do you restart a computer?":"Computer software",
+    }
+
+    

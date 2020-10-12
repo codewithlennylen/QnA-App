198747c6cf1da3cd12b321e1eb420ddb50c70fd5
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-10-12 22:14:02 +0300

    Modified dictionary object

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 questions = {
-    "How do you restart a computer?": "Networking",
-    "What programming language should i learn": "Computer programming",
-    "What is linux": "Computing",
-    "Is pirate-bay illegal": "Computer software",
-    "The blue screen of death!": "Computer software",
-    "What is linux?": "Computing",
-    "How do you restart a computer??": "Computer software",
-    "How do you access the dark web?": "Networking",
-    "Which is the best VPN to use?": "Security",
-    "How do I know if my computer has been hacked?": "Security",
-    "Which is the best arcade game to download now?": "Gaming",
-    "Which application can i download to use in listening mp3 music?": "Application"
+    "How do you restart a computer?": ["Networking","Random Answer"],
+    "What programming language should i learn": ["Computer programming"],
+    "What is linux": ["Computing","Random Answer"],
+    "Is pirate-bay illegal": ["Computer software"],
+    "The blue screen of death!": ["Computer software","Random Answer"],
+    "What is linux?": ["Computing","Random Answer"],
+    "How do you restart a computer??": ["Computer software"],
+    "How do you access the dark web?": ["Networking","Random Answer"],
+    "Which is the best VPN to use?": ["Security"],
+    "How do I know if my computer has been hacked?": ["Security"],
+    "Which is the best arcade game to download now?": ["Gaming"],
+    "Which application can i download to use in listening mp3 music?": ["Application","Random Answer"]
 }
 
 

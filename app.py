937ace86af916a8b14f9510462d36c7c44caff04
Hilario-gomez <71901200+Hilario-gomez@users.noreply.github.com>
2020-10-21 19:21:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,10 @@ def ask():
 def view():
     return render_template('view.html')
 
+@app.route('/Register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
